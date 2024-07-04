979f2599b03c638ec5c1b03675c7d2efc4f83770
ms-shashank <shashankms2003@gmail.com>
2024-07-05 02:18:44 +0530

    fixed

--- fe.py
+++ fe.py
@@ -13,7 +13,7 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
-st.title(":blue[Sentiment Analysis of a Product]")
+st.title(":blue[Sentiment Analysis of a Product]", anchor=False)
 st.divider()
 
 # Initialize session state variables
@@ -45,6 +45,22 @@ def run_script(user_input):
     trained_data, imageUrl, ratings, figs = ultra_main(user_input, session_id)
     return trained_data, imageUrl, ratings, figs 
 
+def clear_session_state():
+    st.session_state.sentiData = None
+    st.session_state.imageUrl = None
+    st.session_state.Ratings = None
+    st.session_state.figs = None
+    
+def delete_csv_files():
+    session_id = st.session_state.session_id
+    review_filename = f"reviews_{session_id}.csv"
+    rating_filename = f"star_ratings_{session_id}.csv"
+    
+    if os.path.exists(review_filename):
+        os.remove(review_filename)
+    if os.path.exists(rating_filename):
+        os.remove(rating_filename)
+    
 form = st.form(key="input_form", clear_on_submit=True)
 text = form.text_input("Enter a product name: ")
 submit = form.form_submit_button(label="Search")
@@ -55,8 +71,12 @@ if submit:
     else:
         with st.spinner(f"Searching for reviews for **{text}**..."):
             # clear_data()
-            st.session_state['sentiData'], st.session_state['imageUrl'], st.session_state['Ratings'], st.session_state['figs'] = run_script(text)
-
+            try:
+                clear_session_state()
+                st.session_state['sentiData'], st.session_state['imageUrl'], st.session_state['Ratings'], st.session_state['figs'] = run_script(text)
+            except Exception as e:
+                st.error(f"An error occured {e}")
+                
 if st.session_state['sentiData'] is not None:
     col1, col2 = st.columns(2)
     with col1:
@@ -88,4 +108,4 @@ if st.session_state['sentiData'] is not None:
     with col5:
         st.pyplot(st.session_state['figs'][2])
 
-            
+    delete_csv_files()        
--- scrapy.py
+++ scrapy.py
@@ -6,34 +6,20 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-# import rembg
+import random
 
 userAgents = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
-        'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
-        ]
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
+]
 
-headers = {
+headers_template = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
     "Accept-Encoding": "gzip, deflate, br, zstd", 
     "Accept-Language": "en-US,en;q=0.5", 
@@ -44,139 +30,66 @@ headers = {
     "Sec-Fetch-User": "?1", 
     "Sec-Gpc": "1", 
     "Upgrade-Insecure-Requests": "1", 
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0", 
     "X-Amzn-Trace-Id": "Root=1-667c53fe-22aa4a273644bb2924a2fea4"
-  } 
+}
 
 rating = []
-reviews = []
-fiveStarRating = []
-fourStarRating = []
-threeStarRating = []
-twoStarRating = []
-oneStarRating = []
 
 def scraping_top_url(user_input, session_id):
-    # user_input = input("Enter a product: ")
     url = top_product_url(user_input)
-    # print(url)
-    img_url ,star, fiveStarReview, fourStarReview, threeStarReview, twoStarReview, oneStarReview = scraping_rating_and_reviews(url, session_id)
+    img_url, star, fiveStarReview, fourStarReview, threeStarReview, twoStarReview, oneStarReview = scraping_rating_and_reviews(url, session_id)
     return img_url, star, fiveStarReview, fourStarReview, threeStarReview, twoStarReview, oneStarReview      
-    # scraping_reviews(url)
 
 def scraping_rating_and_reviews(rating_url, session_id):
-    
-    max_tries = 21
-    attempts = 0
+    retries = 7
     response = None
-
-    fiveStarUrl = 0
-    fourStarUrl = 0 
-    threeStarUrl = 0
-    twoStarUrl = 0
-    oneStarUrl = 0
-
-    while attempts < max_tries and not response:
-        for i in userAgents:
-            try:
-                response = requests.get(rating_url, headers)
-                if response.status_code == 200:
-                    # print(i)
-                    break
-            except requests.RequestException as e:
-                print(f"An error occurred: {e}")
-            time.sleep(0.100)
-        attempts += 1
+    image_link = ""
     
-    # with requests.Session() as session
-    # with requests.Session() as session:
-    #     session.headers.update({'User-Agent': userAgents[0]})
-    #     response = session.get(rating_url)
-    if response.ok:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-    #     stars =  soup.find_all("a", class_ = "a-size-base a-link-normal")
-    #     print(stars)
-    #     for precentage in stars:
-    #         rating.append(precentage.get_text())
-    #     print(rating)
-    # else:
-    #     print("Failed to retrieve the ratings after maximum retries.")
-        img = soup.find("img", id = "landingImage")
-        image_link = img['src']
-        # input_image = Image.open(image_link)
-        # output_image = remove(input_image)
+    for _ in range(retries):
+        headers = headers_template.copy()
+        headers["User-Agent"] = random.choice(userAgents)
+        response = requests.get(rating_url, headers=headers)
+        if response.status_code == 200:
+            break
 
-        #print(image_link)
+    if response and response.ok:
+        soup = BeautifulSoup(response.content, "html.parser")
+        try:
+            img = soup.find("img", id="landingImage") or \
+                soup.find("img", class_="a-dynamic-image a-stretch-horizontal") or \
+                soup.find("img", {"data-old-hires": True}) or \
+                soup.find("img", src=True)
+                
+            if img:
+                image_link = img["src"]
+        except Exception as e:
+            print(e)
 
-        x = soup.find_all("td", class_ = "a-text-right a-nowrap a-nowrap")
-        for td in x:
-            rating_percentage = td.find_all("a", class_ = "a-size-base a-link-normal")
-            zero_percentage = td.find_all("span", class_ = "a-size-base")
+        star = soup.find_all("td", class_="a-text-right a-nowrap a-nowrap")
+        for td in star:
+            rating_percentage = td.find_all("a", class_="a-size-base a-link-normal")
+            zero_percentage = td.find_all("span", class_="a-size-base")
             for zero in zero_percentage:
                 rating.append(zero.get_text())
             for percentage in rating_percentage:
                 rating.append(percentage.get_text())
-        # print(rating)
 
         fiveStarReviewLink = soup.find_all('a', {'class': "a-size-base a-link-normal"})
-        #print(fiveStarReviewLink)
+        fiveStarUrl = fourStarUrl = threeStarUrl = twoStarUrl = oneStarUrl = ""
         if fiveStarReviewLink:
-
-            # fiveStarUrl = ""
-            # fourStarUrl = ""
-            # threeStarUrl = ""
-            # twoStarUrl = ""
-            # oneStarUrl = ""
-
-            # itterate through the html parse and i can handle everything/retrieve any data from the html 
             for hyperlinks in fiveStarReviewLink:
-                # every parameter till query string separater "?" it is same so we will search only for those hyper links 
                 if 'ref=acr_dp_hist_5?' in hyperlinks['href']:
-                    #print(hyperlinks['href'])
-                    fiveStar = hyperlinks['href']
-                    fiveStarUrl = 'https://www.amazon.in' + fiveStar
-
-
+                    fiveStarUrl = 'https://www.amazon.in' + hyperlinks['href']
                 if 'ref=acr_dp_hist_4?' in hyperlinks['href']:
-                    fourStar = hyperlinks['href']
-                    fourStarUrl = 'https://www.amazon.in' + fourStar
-
-
+                    fourStarUrl = 'https://www.amazon.in' + hyperlinks['href']
                 if 'ref=acr_dp_hist_3?' in hyperlinks['href']:
-                    threeStar = hyperlinks['href']
-                    threeStarUrl = 'https://www.amazon.in' + threeStar
-
-
+                    threeStarUrl = 'https://www.amazon.in' + hyperlinks['href']
                 if 'ref=acr_dp_hist_2?' in hyperlinks['href']:
-                    twoStar = hyperlinks['href']
-                    twoStarUrl = 'https://www.amazon.in' + twoStar
-
-
+                    twoStarUrl = 'https://www.amazon.in' + hyperlinks['href']
                 if 'ref=acr_dp_hist_1?' in hyperlinks['href']:
-                    oneStar = hyperlinks['href']
-                    oneStarUrl = 'https://www.amazon.in' + oneStar
-                    break
+                    oneStarUrl = 'https://www.amazon.in' + hyperlinks['href']
 
-
-        # fiveStarUrl = 'https://www.amazon.in' + fiveStar
-        # fourStarUrl = 'https://www.amazon.in' + fourStar
-        # threeStarUrl = 'https://www.amazon.in' + threeStar
-        # twoStarUrl = 'https://www.amazon.in' + twoStar
-        # oneStarUrl = 'https://www.amazon.in' + oneStar
-        
-        # print(fiveStarUrl)
-        # print(fourStarUrl)
-        # print(threeStarUrl)
-        # print(twoStarUrl)
-        # print(oneStarUrl)
-    else:
-        print("Response not recivied/Satisifyed")
-
-#------------------------------------IMPORTANT---------------------------------
-    # Convert percentages to float for graphing
     star_percentages = [float(rates.strip('%')) for rates in rating]
-
     st = ['5 stars', '4 stars', '3 stars', '2 stars', '1 star']
 
     with open(f'star_ratings_{session_id}.csv', 'w', newline='') as file:
@@ -187,17 +100,7 @@ def scraping_rating_and_reviews(rating_url, session_id):
 
     rating.clear()
 
-    # plt.bar(st, star_percentages, color="skyblue")
-    # plt.xlabel("Stars")
-    # plt.ylabel("percentage")
-    # plt.title("Star Ratings")
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    # plt.show(block=False)
-    # plt.pause(3)
-    # plt.close()
-
     return image_link, rating, fiveStarUrl, fourStarUrl, threeStarUrl, twoStarUrl, oneStarUrl      
 
 if __name__ == "__main__":
-    scraping_top_url()
+    scraping_top_url("iphone 15 pro max", "blah blah")
